ed55e451ddc763b3d4c61f879ba387d1274ce8f8
martindurantdf <104039754+martindurantdf@users.noreply.github.com>
2022-05-03 10:01:16 -0300

    Con enunciado

--- src/datos.ts
+++ src/datos.ts
@@ -1,3 +1,12 @@
+/*
+•Desarrolle un algoritmo que diga el precio de
+una compra
+• La compra se compone del precio del producto
+y la cantidad
+•Si el cliente gasta más de $1000 debemos
+aplicarle un descuento del 10%
+*/
+
 let btnAgregar = document.getElementById("btnAgregar");
 let btnCalc = document.getElementById("btnCalcular");
 let precio = document.getElementById("precio");
